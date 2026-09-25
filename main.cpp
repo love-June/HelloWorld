48a039fe2adaf9850a464d986b89b7f69c80bdac
love-June <1273248486@qq.com>
2026-09-25 09:13:37 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+int main()
+{
+    int arr[] = {12, 23, 34, 45, 56};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, size);
+    return 0;
+}
